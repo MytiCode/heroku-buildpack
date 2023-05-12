b72a10cb477e97474e56f8ea5e395f4ece225a44
xisforxerxes <ouellr@gmail.com>
2023-05-11 22:10:11 -0400

    Take more drastic steps

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -10,7 +10,7 @@ pnpm_node_modules() {
   local build_dir=${1:-}
   if [ -e "$build_dir/package.json" ]; then
     cd "$build_dir" || return
-    pnpm install 2>&1
+    NODE_ENV= pnpm install 2>&1
   else
     echo "Skipping (no package.json)"
   fi
@@ -18,9 +18,6 @@ pnpm_node_modules() {
 
 pnpm_prune_devdependencies() {
   local build_dir=${1:-}
-  if [[ "$NPM_CONFIG_PRODUCTION" == "true" ]] ; then
-      production="true"
-      cd "$build_dir" || return
-      pnpm prune 2>&1
-  fi
+  cd "$build_dir" || return
+  pnpm prune 2>&1
 }
